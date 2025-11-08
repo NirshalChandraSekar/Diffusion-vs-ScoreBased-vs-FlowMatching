5e62e3f454110231d2f3edeb925a0827e24d5c2f
NirshalChandraSekar <nirshalniru@gmail.com>
2025-11-08 00:18:40 -0600

    completed diffusion and score based models

--- hw3_step1_main.py
+++ hw3_step1_main.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from tqdm.auto import tqdm
 import step1_utils.utils as utils
 import argparse
+import numpy as np
 
 class Config:
     def __init__(self):
@@ -16,11 +17,11 @@ class Config:
         self.parser.add_argument('--dataset', type=str, default='ffhq', help='either choose ffhq or imagenet')
         
         # hyperparameters for sampling
-        self.parser.add_argument('--total_instances', type=int, default=10, help='number of images you want to generate - 10 is ideal')
+        self.parser.add_argument('--total_instances', type=int, default=1, help='number of images you want to generate - 10 is ideal')
         self.parser.add_argument('--diff_timesteps', type=int, default=1000, help='Original number of steps from Ho et al. (2020) which is 1000 - do not change')
-        self.parser.add_argument('--desired_timesteps', type=int, default=1000, help='How many steps do you want?')
-        self.parser.add_argument('--eta', type=float, default=0.0, help='Should be between [0.0, 1.0]')
-        self.parser.add_argument('--schedule', type=str, default="1000", help="regular/irregular schedule to use (jumps)")
+        self.parser.add_argument('--desired_timesteps', type=int, default=100, help='How many steps do you want?')
+        self.parser.add_argument('--eta', type=float, default=1.0, help='Should be between [0.0, 1.0]')
+        self.parser.add_argument('--schedule', type=str, default="100", help="regular/irregular schedule to use (jumps)")
 
     def parse(self, args=None):
         """Parse the configuration"""
@@ -41,32 +42,65 @@ class Sampler():
         self.alphas_cumprod = torch.cumprod(self.alphas, axis=0)
         self.alphas_cumprod_prev = torch.cat((torch.tensor([1.0]), self.alphas_cumprod[:-1]))
     
-    def recreate_alphas(self):
-        use_timesteps = utils.space_timesteps(self.conf.diff_timesteps, self.conf.schedule) # Selects a subset of timesteps according to the given schedule
-        self.timestep_map = []
-        last_alpha_cumprod = 1.0
+    # def recreate_alphas(self):
+    #     use_timesteps = utils.space_timesteps(self.conf.diff_timesteps, self.conf.schedule) # Selects a subset of timesteps according to the given schedule
+    #     self.timestep_map = []
+    #     last_alpha_cumprod = 1.0
         
-        # TODO: Initialize an empty list to store the new beta values --> This is to collect the new betas corresponding to the chosen timesteps.
-        new_betas = []
-        for i, alpha_cumprod in enumerate(self.alphas_cumprod):
+    #     # TODO: Initialize an empty list to store the new beta values --> This is to collect the new betas corresponding to the chosen timesteps.
+    #     new_betas = []
+    #     for i, alpha_cumprod in enumerate(self.alphas_cumprod):
             
-            # TODO: Check if the current timestep index 'i' is part of the selected timesteps (use_timesteps)
-            if i in use_timesteps:
-                # TODO: If so, compute the corresponding beta value and append it to the empty list
-                new_beta = 1 - (alpha_cumprod / last_alpha_cumprod)
-                new_betas.append(new_beta)
-                # TODO: Update 'last_alpha_cumprod' to the current 'alpha_cumprod'
-                last_alpha_cumprod = alpha_cumprod
-                # TODO: Keep track of which original timesteps are being used by appending 'i' to 'self.timestep_map'
-                self.timestep_map.append(i)
+    #         # TODO: Check if the current timestep index 'i' is part of the selected timesteps (use_timesteps)
+    #         if i in use_timesteps:
+    #             # TODO: If so, compute the corresponding beta value and append it to the empty list
+    #             new_beta = 1 - (alpha_cumprod / last_alpha_cumprod)
+    #             new_betas.append(new_beta)
+    #             # TODO: Update 'last_alpha_cumprod' to the current 'alpha_cumprod'
+    #             last_alpha_cumprod = alpha_cumprod
+    #             # TODO: Keep track of which original timesteps are being used by appending 'i' to 'self.timestep_map'
+    #             self.timestep_map.append(i)
 
-        # TODO: Convert 'new_betas' into a PyTorch tensor and store it in 'self.betas'
-        self.betas = torch.tensor(new_betas, dtype=torch.float64)
-        # TODO: After updating betas, Recompute the related alpha terms to refresh alpha values
-        # Hint: A helper function is already implemented in this hw3_step1_main.py file to refresh the alpha values
-        # Understand which function does that and use it here.
+    #     # TODO: Convert 'new_betas' into a PyTorch tensor and store it in 'self.betas'
+    #     self.betas = torch.tensor(new_betas, dtype=torch.float64)
+    #     # TODO: After updating betas, Recompute the related alpha terms to refresh alpha values
+    #     # Hint: A helper function is already implemented in this hw3_step1_main.py file to refresh the alpha values
+    #     # Understand which function does that and use it here.
+    #     self.alpha_init()
+    #     return torch.tensor(self.timestep_map)
+
+    def recreate_alphas(self):
+        kept_indices = utils.space_timesteps(self.conf.diff_timesteps, self.conf.schedule)
+
+        if isinstance(kept_indices, set):
+            kept_indices = sorted(list(kept_indices))
+        elif hasattr(kept_indices, 'tolist'):
+            kept_indices = kept_indices.tolist()
+        else:
+            kept_indices = list(kept_indices)
+            
+        self.timestep_map = kept_indices
+
+        new_betas = []
+        last_alpha_cumprod = torch.tensor(1.0, dtype=self.alphas.dtype, device=self.alphas.device)
+
+        alpha_bar = self.alphas_cumprod
+
+        for idx in kept_indices:
+            curr_alpha_cumprod = alpha_bar[idx]
+            beta = 1.0 - (curr_alpha_cumprod / last_alpha_cumprod)
+
+            beta = torch.clamp(beta, min=1e-12, max=1 - 1e-12)
+
+            new_betas.append(beta)
+            last_alpha_cumprod = curr_alpha_cumprod
+
+        self.betas = torch.stack(new_betas).to(dtype=self.alphas.dtype, device=self.alphas.device)
+
         self.alpha_init()
-        return torch.tensor(self.timestep_map)
+
+        return torch.tensor(self.timestep_map, dtype=torch.long)
+
     
     def get_variance(self, x, t):
         posterior_variance = (self.betas * (1.0 - self.alphas_cumprod_prev) / (1.0 - self.alphas_cumprod))
@@ -86,8 +120,15 @@ class Sampler():
         ############################
         # TODO: Implement the function predicting the clean denoised estimate x_{0|t} --> step (c and d)
         ############################
-        x0_hat = None
-        
+
+        C_t = 1 / torch.sqrt(self.alphas_cumprod)
+        D_t = (torch.sqrt(1 - self.alphas_cumprod)) / (torch.sqrt(self.alphas_cumprod))
+
+        C_t = utils.extract_and_expand(C_t, t, x_t)
+        D_t = utils.extract_and_expand(D_t, t, x_t)
+
+        x0_hat = (C_t * x_t) - (D_t * model_output)
+
         return x0_hat
     
     def sample_ddpm(self, score_model, x_t, model_t, t):
@@ -105,21 +146,44 @@ class Sampler():
         # Note that extract_and_expand() function already selects the index you input to the function (always input t)
         ############################
 
-        A_t = 1 / torch.sqrt(self.alphas)
-        B_t = - (1 - self.alphas) / (torch.sqrt(1-self.alphas_cumprod) * torch.sqrt(self.alphas))
+        # A_t = 1 / torch.sqrt(self.alphas)
+        # B_t = - (1 - self.alphas) / (torch.sqrt(1-self.alphas_cumprod) * torch.sqrt(self.alphas))
+
+        # A_t = utils.extract_and_expand(A_t, t, x_t)
+        # B_t = utils.extract_and_expand(B_t, t, x_t)
+
+        # mean = A_t * x_t + B_t * model_output
+
+        x_0_hat = self.predict_x0_hat(x_t, t, model_output)
+
+        A_t = (torch.sqrt(self.alphas)*(1 - self.alphas_cumprod_prev)) / (1 - self.alphas_cumprod)
+        B_t = (torch.sqrt(self.alphas_cumprod_prev)*(1 - self.alphas)) / (1 - self.alphas_cumprod)
 
         A_t = utils.extract_and_expand(A_t, t, x_t)
         B_t = utils.extract_and_expand(B_t, t, x_t)
 
-        mean = A_t * x_t + B_t * model_output
+        mean = A_t * x_t + B_t * x_0_hat
 
         # σ_t (log variance → variance → stddev)
         sigma = torch.exp(0.5 * model_log_variance) 
 
-        add_noise = (t > 0).view(-1, *([1] * (x_t.dim() - 1))).to(x_t.dtype)
-        z = torch.randn_like(x_t) * add_noise
+        if(t == 0):
+            z = torch.zeros_like(x_t)
+        else:
+            # create random noise in numpy and convert to torch to see if choosing random Z makes the model stochastic
+            # z = torch.from_numpy(np.random.randn(*x_t.shape)).to(x_t.device).to(x_t.dtype)
+
+            z = torch.randn_like(x_t)
 
         sample = mean + sigma * z
+
+        # if(t == 999 or t == 750 or t == 500 or t == 250 or t==50 or t==0):
+        #     #save the intermediate images
+        #     x0_hat = self.predict_x0_hat(x_t, t, model_output)
+        #     image_filename = f"intermediate_image_t{t.item()}.png"
+        #     image_path = os.path.join(self.conf.out_path, "intermediate_images", image_filename)
+        #     os.makedirs(os.path.dirname(image_path), exist_ok=True)
+        #     plt.imsave(image_path, utils.clear_color(x0_hat))
 
         return sample
 
@@ -131,8 +195,30 @@ class Sampler():
         ############################
         # TODO: Implement DDIM sampling --> step (f)
         ############################
-        sample = None
-        
+
+        x_0_hat = self.predict_x0_hat(x_t, t, model_output)
+
+        alpha_bar_t = utils.extract_and_expand(self.alphas_cumprod, t, x_t)
+        alpha_bar_prev = utils.extract_and_expand(self.alphas_cumprod_prev, t, x_t)
+
+        eta = self.conf.eta
+
+        sigma_t = eta * torch.sqrt(
+            ((1 - alpha_bar_prev) / (1 - alpha_bar_t)) * (1 - (alpha_bar_t / alpha_bar_prev))
+        )
+        sigma_t = torch.clamp(sigma_t, min=0.0)
+
+        coeff_eps = torch.sqrt(
+            torch.clamp((1 - alpha_bar_prev) - sigma_t**2, min=0.0)
+        )
+
+        if (t == 0).all():
+            z = torch.zeros_like(x_t)
+        else:
+            z = torch.randn_like(x_t)
+
+        sample = torch.sqrt(alpha_bar_prev) * x_0_hat + coeff_eps * model_output + sigma_t * z
+                
         return sample
     
 def main():
@@ -140,7 +226,7 @@ def main():
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     conf = Config().parse()
     
-    algo = "DDPM"
+    algo = "DDIM"
     if algo == "DDIM":
         print('*' * 60 + f'\nSTARTED {algo} Sampling with eta = \"%.1f\" \n' %conf.eta)
     if algo == "DDPM":
@@ -156,6 +242,7 @@ def main():
     for instance in range(conf.total_instances): 
         sampler_operator = Sampler()
         x_t = utils.get_noise_x_t(instance, conf.total_instances, device)
+        
         pbar = (list(range(conf.desired_timesteps))[::-1])
         
         if conf.desired_timesteps == 1000:
